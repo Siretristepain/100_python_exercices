66a88b11243db34093f11caa3f1b03c6a7866c46
Siretristepain <raphael.ollier7@gmail.com>
2023-08-02 11:47:16 +0200

    Ajout de l'exercice 61

---
+++ exercices_61to65.py
@@ -0,0 +1,19 @@
+#_____________________________
+# Exercice 61 : lecture d'un fichier
+
+# def lireFichier(chemin):
+#     with open(chemin, 'r') as f:
+#         contenu = f.read()
+#         return contenu
+
+# OU SINON
+
+
+# def lirefichier(chemin):
+#     fichier = open(chemin, 'r')
+#     contenu = fichier.read()
+#     fichier.close() # Il est essentiel de bien fermer le fichier après ouverture, la méthode with open de la fonction précédente permet de prendre ça en compte automatiquement
+#     return contenu
+    
+# if __name__ == '__main__':
+#     print(lirefichier(r"C:\Users\Utilisateur\Desktop\100_python_exercices\fichiertest_ex61.txt"))
